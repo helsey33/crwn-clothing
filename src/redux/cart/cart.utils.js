75a1261f86a54f1bafff0a102e3d8848915cc886
Himanshu Ranjan <helseysin827@gmail.com>
2019-08-28 13:37:22 +0530

    created cart item and connected cart dropdown through store

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,5 +10,5 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     );
   }
 
-  return [...cartItems, { cartItemToAdd, quantity: 1 }];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
